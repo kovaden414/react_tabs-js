cd65b332c362e613223eb1dbfff59beea08810fe
Denys Kovalchuk <denkov414@gmail.com>
2024-12-18 01:00:26 +0200

    fixed mistakes

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,34 +1,39 @@
 import cn from 'classnames';
 
-export const Tabs = ({ tabs, activeTab, activeTabId, onTabSelected }) => (
-  <div data-cy="TabsComponent">
-    <div className="tabs is-boxed">
-      <ul>
-        {tabs.map(tab => (
-          <li
-            className={cn({
-              'is-active': activeTabId === tab.id,
-            })}
-            data-cy="Tab"
-          >
-            <a
-              href={`#${tab.id}`}
-              data-cy="TabLink"
-              onClick={() => {
-                if (activeTabId !== tab.id) {
-                  onTabSelected(tab.id);
-                }
-              }}
-            >
-              {tab.title}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
 
-    <div className="block" data-cy="TabContent">
-      {activeTab.content}
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              className={cn({
+                'is-active': activeTab.id === tab.id,
+              })}
+              data-cy="Tab"
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={() => {
+                  if (activeTabId !== tab.id) {
+                    onTabSelected(tab.id);
+                  }
+                }}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {activeTab.content}
+      </div>
     </div>
-  </div>
-);
+  );
+};
